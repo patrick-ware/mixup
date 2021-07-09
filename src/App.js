e1e782df72debca6a56a8864b2d21c339beedf4f
Patrick Ware <patrick.r.ware@gmail.com>
2021-07-09 16:16:29 -0700

    fetch operation working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData]= useState([]);
+
+  // fetch data from API
+  function doFetch(){
+    const api = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka"
+
+    fetch(api)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error ('Network response was not ok');      
+        } 
+        return response.json()
+      })
+      .then(data => {
+        console.log("this is data", data)
+          setData(data.drinks)
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+   }
+
+  // useEffect hooks
+  useEffect(doFetch,[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Content!
     </div>
   );
 }
